88b64522eb25893e5e616c375fae661552a478a9
Junior Gassam <gassamgassamlionel@gmail.com>
2025-04-15 12:16:33 +0200

    test hook OK

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,26 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('var')
-;
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
-return (new PhpCsFixer\Config())
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->name('*.php')
+    ->exclude('var')
+    ->exclude('vendor');
+
+return (new Config())
+    ->setRiskyAllowed(true) // autorise les fixers plus stricts
     ->setRules([
+        '@PSR12' => true,
         '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'align_single_space'],
+        'blank_line_before_statement' => ['statements' => ['return']],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'left'],
+        'no_unused_imports' => true,
     ])
     ->setFinder($finder)
-;
-
+    ->setUsingCache(true);
