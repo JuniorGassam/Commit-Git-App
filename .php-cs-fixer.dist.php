4fef5f1d9a506f05ec22ad929d63c1923275eb24
Junior Gassam <gassamgassamlionel@gmail.com>
2025-04-15 12:03:39 +0200

    Test hook manuel

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,21 +5,10 @@ $finder = (new PhpCsFixer\Finder())
     ->exclude('var')
 ;
 
-/*return (new PhpCsFixer\Config())
+return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
     ])
     ->setFinder($finder)
-;*/
+;
 
-
-return PhpCsFixer\Config::create()
-    ->setRules([
-        '@PSR12' => true,
-        'camel_case_variables' => true,
-        'phpdoc_no_alias_tag' => false,
-    ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->in(__DIR__)
-    );
